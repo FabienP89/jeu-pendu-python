bb51de325a8dca6892d84ca20fc5e24ab67417d5
Fabien Plateau <fabien.plateau@hotmail.fr>
2020-01-16 14:27:09 +0100

    ajout du menu principal dans jeu_pendu.py qui remplace l'option rejouer

--- jeu_pendu.py
+++ jeu_pendu.py
@@ -5,53 +5,46 @@ from fonction_pendu import trouver_lettre
 from fonction_pendu import cacher_mot
 from mots_pendu import mots
 
-    
-nombre_essais = 1
 
-essais = 0
+nombre_essais = 10
+jouer = True
 
-mot = choisir_mot()
-
-print(f'vous avez le droit à {nombre_essais} essais ! Bonne chance !')
-
-lettres_trouvees = []
-
-lettres_utilisees = []
-
-while essais < nombre_essais :
-    if len(lettres_utilisees) > 0 :
-        print(f"Vous avez déjà cherché les lettres suivantes : {lettres_utilisees}")
-    lettre_utilisateur = entrer_lettre()
-    if lettre_utilisateur in lettres_utilisees :
-        print(f"Attention, vous avez déjà utilisé la lettre '{lettre_utilisateur}' !")
+while jouer :
+    choix = input("Souhaitez-vous faire une partie ? o/n ")
+    if choix == "o":
+        # on initialise le jeu
+        essais = 0
+        mot = choisir_mot()
+        lettres_trouvees = []
+        lettres_utilisees = []
+        print(f'vous avez le droit à {nombre_essais} essais ! Bonne chance !')
+    elif choix == "n":
+        print("A bientôt :)")
+        exit()
+    else :
+        # on réaffiche le message jusqu'a ce que l'internaute effectue une saisie valide
+        print("Veuillez saisir 'o' ou 'n'")
         continue
-    lettres_utilisees.append(lettre_utilisateur)
-    nouvelle_lettre = trouver_lettre(lettre_utilisateur, mot)
-    lettres_trouvees.append(nouvelle_lettre)
-    mot_cacher = cacher_mot(mot, lettres_trouvees)
-    print(mot_cacher)
-    if mot == mot_cacher :
-        print(f"Bravo vous avez gagné, le mot mystère était {mot}!")
-        break
-    essais +=1
-    essais_restant = nombre_essais - essais 
-    print(f"Il vous reste {essais_restant} essais !")
-    if essais == nombre_essais :
-        print(f"Perdu ! le mot mystère était : '{mot}'!")
-        choix = input("Souhaitez-vous retentez votre chance ? o/n ")
-        while choix :
-            if choix == "o":
-                # on réinitialise le jeu et on sort de la boucle
-                mot = choisir_mot()
-                essais = 0
-                lettres_trouvees = []
-                lettres_utilisees = []
-                break
-            elif choix == "n":
-                print("A bientôt :)")
-                exit()
-            else :
-                # on réaffiche le message jusqu'a ce que l'internaute effectue une saisie valide
-                print("Veuillez saisir 'o' ou 'n'")
-                choix = input("Souhaitez-vous retentez votre chance ? o/n ")
-        
+
+
+
+    while essais < nombre_essais :
+        if len(lettres_utilisees) > 0 : # aide mémoire pour l'utilisateur : indique les lettres déjà cherchées
+            print(f"Vous avez déjà cherché les lettres suivantes : {lettres_utilisees}")
+        lettre_utilisateur = entrer_lettre()
+        if lettre_utilisateur in lettres_utilisees : # alerte l'utilisateur en cas de doublon de saisie d'une lettre
+            print(f"Attention, vous avez déjà utilisé la lettre '{lettre_utilisateur}' !")
+            continue
+        lettres_utilisees.append(lettre_utilisateur)
+        nouvelle_lettre = trouver_lettre(lettre_utilisateur, mot)
+        lettres_trouvees.append(nouvelle_lettre)
+        mot_cacher = cacher_mot(mot, lettres_trouvees)
+        print(mot_cacher)
+        if mot == mot_cacher :
+            print(f"Bravo vous avez gagné, le mot mystère était {mot} !")
+            break # on sort de la boucle while actuelle pour revenir à la boucle jouer
+        essais +=1 
+        essais_restant = nombre_essais - essais
+        print(f"Il vous reste {essais_restant} essais !")
+        if essais == nombre_essais :
+            print(f"Perdu ! le mot mystère était : '{mot}'!")
